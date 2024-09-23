0605ccc6bd24cf786a6c57c4d85576dce9033524
Ishita <144627073+elekish@users.noreply.github.com>
2024-09-23 16:47:12 +0530

    Update characteristics.py

--- characteristics.py
+++ characteristics.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from scipy.stats import kurtosis,skew
 
 
 def calculate_statistics(data_3d):
@@ -24,5 +25,64 @@ def calculate_statistics(data_3d):
 
     return means, std_devs
 
+def flatten_3d_to_2d(array_3d):
+    if array_3d.ndim != 3:
+        raise ValueError("Input array must be 3D")
+    flattened_arrays = np.array([array_3d[i].flatten() for i in range(array_3d.shape[0])])
+
+    return flattened_arrays
+
+def z_normalize(array_1d, mean, std_dev):
+    return (array_1d - mean) / std_dev
 
 
+def calculate_normalized_variance_and_kurtosis(data):
+    data = np.asarray(data, dtype=float)  # Convert to float, handles None
+    data = np.nan_to_num(data)
+    normalized_variance = np.var(data, ddof=1)  # Sample variance
+    kurt_value = kurtosis(data)
+    return normalized_variance, kurt_value
+
+def process_batches_for_normalised(array_1d, batch_size=200):
+    num_batches = len(array_1d) // batch_size
+    variances = []
+    kurtoses = []
+
+    for i in range(num_batches):
+        batch = array_1d[i * batch_size:(i + 1) * batch_size]
+        var, kurt = calculate_normalized_variance_and_kurtosis(batch)
+        variances.append(var)
+        kurtoses.append(kurt)
+
+    return np.array(variances), np.array(kurtoses)
+
+def calculate_statistics_in_batches(data):
+    # Ensure data is numeric and replace NaN/None with 0
+    data = np.asarray(data, dtype=float)  # Convert to float, handles None
+    data = np.nan_to_num(data)  # Replace NaN with 0
+
+    # Calculate mean, standard deviation, variance, and skewness
+    mean_value = np.mean(data)
+    std_dev_value = np.std(data, ddof=1)  # Sample std deviation
+    variance_value = np.var(data, ddof=1)  # Sample variance
+    skewness_value = skew(data)
+
+    return mean_value, std_dev_value, variance_value, skewness_value
+
+def process_batches_raw(array_1d, batch_size=200):
+    num_batches = len(array_1d) // batch_size
+    means = []
+    stddevs=[]
+    variances=[]
+    skews=[]
+
+    for i in range(num_batches):
+        batch = array_1d[i * batch_size:(i + 1) * batch_size]
+        mm, sstd, varr, skewness = calculate_statistics_in_batches(batch)
+        variances.append(varr)
+        means.append(mm)
+        stddevs.append(sstd)
+        skews.append(skewness)
+
+
+    return np.array(means), np.array(stddevs), np.array(variances), np.array(skews)
